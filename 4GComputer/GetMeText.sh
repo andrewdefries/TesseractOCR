79872db2691a1ec69900466cc8714e9eba820a15
andrewdefries <andrew.defries@gmail.com>
2014-05-03 06:23:04 +0000

    works now

--- 4GComputer/GetMeText.sh
+++ 4GComputer/GetMeText.sh
@@ -1,10 +1,17 @@
 ############
+############
+#gsutil cp -R gs://pesticides2crunch/PlantSubstances/Batch1 .
+#gsutil cp -R gs://pesticides2crunch/SecondaryMetabolism .
+###########
+cp *.sh SecondaryMetabolism 
+cd SecondaryMetabolism
+###########
 rm *.ready.jpg
 rm *.rotated.jpg
 rm *.crop.jpg
-############
-gsutil ls gs://pesticides2crunch/PlantSubstances/Batch1 |  sed -n '/.*[02468]\.jpg/p' > Even
-gsutil ls gs://pesticides2crunch/PlantSubstances/Batch1 |  sed -n '/.*[13579]\.jpg/p' > Odd
+###########
+ls | sed -n '/.*[02468]\.jpg/p' > Even
+ls | sed -n '/.*[13579]\.jpg/p' > Odd
 ############
 even=(`cat Even`)
 odd=(`cat Odd`)
@@ -21,12 +28,13 @@ do
 #rotate, crop
 #get values from imagej use record to see makeRectangle(608,56,3580,2668)
 echo $i
-convert $i -crop 3580x2668+608+56 $i.crop.jpg 
-convert -rotate -90 $i.crop.jpg $i.rotated.jpg
+convert $i -crop 2948x1692+760+660 $i.crop.jpg 
+rm $i
+convert -rotate 90 $i.crop.jpg $i.rotated.jpg
 rm  $i.crop.jpg
 #textcleaner
-#./textcleaner.sh $i.rotated.jpg $i.ready.jpg
-./textcleaner.sh -g -e stretch -f 25 -o 5 -s 1 $i.rotated.jpg $i.ready.jpg
+./textcleaner.sh $i.rotated.jpg $i.ready.jpg
+#./textcleaner.sh -g -e stretch -f 25 -o 5 -s 1 $i.rotated.jpg $i.ready.jpg
 #tesseract time
 rm $i.rotated.jpg
 #rm $i.ready.jpg
@@ -48,11 +56,12 @@ do
 #rotate, crop
 #get values from imagej use record to see makeRectangle(608,56,3580,2668)
 echo $i
-convert $i -crop 3384x2652+736+200 $i.crop.jpg 
-convert -rotate 90 $i.crop.jpg $i.rotated.jpg
+convert $i -crop 3152x1864+520+496 $i.crop.jpg 
+rm $i
+convert -rotate -90 $i.crop.jpg $i.rotated.jpg
 #textcleaner
-#./textcleaner.sh $i.rotated.jpg $i.ready.jpg
-./textcleaner.sh -g -e stretch -f 25 -o 5 -s 1 $i.rotated.jpg $i.ready.jpg
+./textcleaner.sh $i.rotated.jpg $i.ready.jpg
+#./textcleaner.sh -g -e stretch -f 25 -o 5 -s 1 $i.rotated.jpg $i.ready.jpg
 #tesseract time
 tesseract $i.ready.jpg $i
 #   
