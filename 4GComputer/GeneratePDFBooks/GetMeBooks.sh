a67c0f2bc725d0763615e9eba517b15d6ad7c3e9
andrewdefries <andrew.defries@gmail.com>
2014-05-07 22:50:09 +0000

    makes big pdfs

--- 4GComputer/GeneratePDFBooks/GetMeBooks.sh
+++ 4GComputer/GeneratePDFBooks/GetMeBooks.sh
@@ -2,22 +2,32 @@
 gsutil ls gs://pesticides2crunch/Local > WorkList
 
 #Setup variable assignments
-Work=(`cat WorkList`)
+WorkList=(`cat WorkList`)
 
 ##
 for i in "${WorkList[@]}"
 
 do
 ########################
+########################
 #Clean up names
-cat $i | cut -c 30-60 | sed 's/[/]//g' > Book
+#cat $i | sed 's/gs:\///g' | cut -c 30-60 | sed 's/[/]//g' > Book
+echo $i | cut -c 30-60 | sed 's/[/]/\.pdf/g' > Book
 
 Name=(`cat Book`)
 
 #To use on google cloud
-gsutil cp $i/*.jpg.pdf .
+gsutil cp $i*.jpg.pdf .
+
+echo $i
 
 #local
 gs -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE="$Name" -dBATCH *.pdf
 
 rm *.jpg.pdf
+############
+echo "$Name"
+###################
+done
+
+echo "Be sure to copy the books to gs://swap"
