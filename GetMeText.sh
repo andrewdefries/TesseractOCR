69f46332e6ce57c422b598098f3742cc7e58d0cc
Andrew Defries PhD <andrew.defries@gmail.com>
2014-05-06 23:08:42 -0700

    Update GetMeText.sh
    hocr to html and searchable pdf via gscan2pdf

--- GetMeText.sh
+++ GetMeText.sh
@@ -1,4 +1,17 @@
+##gsutil cp -R gs://pesticides2crunch/Local/SecondaryMetabolism .
+###
+##cp *.sh SecondaryMetabolism
+##cd SecondaryMetabolism
+###
+rm img00000.jpg
+rm img00392.jpg
+rm img00393.jpg
+rm img00394.jpg
+rm img00395.jpg
 ############
+rm *.jpg.txt
+rm *.jpg.pdf
+rm *.jpg.html
 rm *.ready.jpg
 rm *.rotated.jpg
 rm *.crop.jpg
@@ -21,13 +34,22 @@ do
 #rotate, crop
 #get values from imagej use record to see makeRectangle(608,56,3580,2668)
 echo $i
-convert $i -crop 3580x2668+608+56 $i.crop.jpg 
-convert -rotate -90 $i.crop.jpg $i.rotated.jpg
+convert $i -crop 2884x1772+756+536 $i.crop.jpg 
+#rm $i
+
+convert -rotate 90 $i.crop.jpg $i.rotated.jpg
+rm $i.crop.jpg
+
 #textcleaner
 #./textcleaner.sh $i.rotated.jpg $i.ready.jpg
 ./textcleaner.sh -g -e stretch -f 25 -o 5 -s 1 $i.rotated.jpg $i.ready.jpg
+rm $i.rotated.jpg
+
 #tesseract time
 tesseract $i.ready.jpg $i
+tesseract $i.ready.jpg $i hocr
+hocr2pdf -i $i.ready.jpg -o $i.pdf < $i.html
+
 #
 done
 ###########
@@ -44,15 +66,22 @@ do
 #rotate, crop
 #get values from imagej use record to see makeRectangle(608,56,3580,2668)
 echo $i
-convert $i -crop 3384x2652+736+200 $i.crop.jpg 
-convert -rotate 90 $i.crop.jpg $i.rotated.jpg
+convert $i -crop 3064x1816+600+540 $i.crop.jpg 
+#rm $i
+convert -rotate -90 $i.crop.jpg $i.rotated.jpg
+rm $i.crop.jpg
+
 #textcleaner
 #./textcleaner.sh $i.rotated.jpg $i.ready.jpg
 ./textcleaner.sh -g -e stretch -f 25 -o 5 -s 1 $i.rotated.jpg $i.ready.jpg
+rm $i.rotated.jpg
+
 #tesseract time
 tesseract $i.ready.jpg $i
+tesseract $i.ready.jpg $i hocr
+hocr2pdf -i $i.ready.jpg -o $i.pdf < $i.html
+
 #   
 done
 ###########
 ###########
-
