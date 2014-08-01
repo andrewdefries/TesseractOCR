a6f67e520328813995770ebd65277f637ebdd785
Andrew Defries PhD <andrew.defries@gmail.com>
2014-08-01 12:16:01 -0700

    run this

---
+++ Easier/Books/DrugsAsTeratogens/GetSampleForUser.sh
@@ -0,0 +1,49 @@
+targetbucket=(`cat TargetBucket`)
+
+gsutil -m mb $Targetbucket
+gsutil -m ls $targetbucket | shuf > ShuffledWorkList
+
+######
+cat ShuffledWorkList | sed -n '/.*[02468]\.jpg/p' | head -n 18 > EvenCropWorkList
+cat ShuffledWorkList | sed -n '/.*[13579]\.jpg/p' |head -n 18 > OddCropWorkList
+######
+
+download4usereven=(`cat EvenCropWorkList`) 
+download4userodd=(`cat OddCropWorkList`) 
+
+
+####
+####
+mkdir Even2Crop
+for m in "${download4usereven[@]}"
+
+do
+####
+cd Even2Crop
+
+gsutil -m cp $m .
+echo "downloaded $m"
+cd ..
+####
+done
+
+
+####
+####
+mkdir Odd2Crop
+for n in "${download4userodd[@]}" 
+
+do
+####
+cd Odd2Crop
+
+gsutil -m cp $n .
+echo "downloaded $n"
+cd ..
+####
+done
+
+
+echo "Now save your crop values in TesseractSpecs"
+
+##pico TesseractSpecs
