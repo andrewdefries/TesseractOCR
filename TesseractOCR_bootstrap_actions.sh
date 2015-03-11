1e2d1c03a3394d3274420e6e9b2a7b02ffd1f58d
Andrew Defries PhD <andrew.defries@gmail.com>
2015-03-10 19:51:05 -0700

    Create TesseractOCR_bootstrap_actions.sh

---
+++ TesseractOCR_bootstrap_actions.sh
@@ -0,0 +1,13 @@
+sudo apt-get install autoconf automake libtool -y
+sudo apt-get install libpng12-dev -y
+sudo apt-get install libjpeg62-dev -y
+sudo apt-get install libtiff4-dev -y
+sudo apt-get install zlib1g-dev -y
+sudo apt-get install libicu-dev -y # (if you plan to make the training tools)
+sudo apt-get install libleptonica-dev -y
+sudo apt-get install tesseract-ocr -y
+
+sudo apt-get install imagemagick -y
+sudo apt-get install graphicsmagick-libmagick-dev-compat -y
+sudo apt-get install exactimage  -y
+sudo apt-get install potrace  -y
